d35718f69b53f7bff5ddbce54f83396cdd6a2258
jyotishpro <kumarjyotish67303@gmail.com>
2024-12-15 20:03:52 +0000

    created binary tress structure

--- Binary_trees/Create_binary.cpp
+++ Binary_trees/Create_binary.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node * left;
+  Node * right;
+  Node(int data ){
+    this->data = data;
+    left = right = NULL;
+  }
+};
+static int idx = -1;
+Node* Binary_trees(vector<int>nodes){
+       idx++;
+       if(nodes[idx]==-1){
+        return NULL;
+       }
+
+       Node * currNode = new Node( nodes[idx]);
+       currNode->left = Binary_trees(nodes);
+       currNode->right = Binary_trees(nodes);
+
+       return currNode;
+
+}
 
 int main(){
 
-    cout<<"this is me";
+    vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* data = Binary_trees(nodes);
+    cout<<data->data;
+
+
     return 0;
 }
