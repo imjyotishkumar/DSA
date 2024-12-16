dc5a714cca6b376d1adf0f9053210764718fe7c6
jyotishpro <kumarjyotish67303@gmail.com>
2024-12-16 23:13:18 +0530

    creted level order traversal

---
+++ Binary_trees/LevelOrderTraversal.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node *Build_tree(vector<int> nodes)
+{
+
+    idx++;
+
+    if (nodes[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node *currNode = new Node(nodes[idx]);
+    currNode->left = Build_tree(nodes);
+    currNode->right = Build_tree(nodes);
+
+    return currNode;
+}
+
+void LevelorderTraversal(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<Node *> Q;
+
+    Q.push(root);
+    Q.push(NULL);
+    while (!Q.empty())
+    {
+
+        Node *curr = Q.front();
+        Q.pop();
+
+        if (curr == NULL)
+        {
+            cout<<endl;
+            if (Q.empty())
+            {
+                break;
+            }
+            Q.push(NULL);
+        }
+        else
+        {
+
+            cout << curr->data;
+            if (curr->left != NULL)
+            {
+                Q.push(curr->left);
+            }
+
+            if (curr->right != NULL)
+            {
+                Q.push(curr->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node *root = Build_tree(nodes);
+    LevelorderTraversal(root);
+
+    return 0;
+}
