a4ed584e5723cc913d7687eef894ef89e9714164
jyotispro <kumarjyotish67303@gmail.com>
2024-12-30 23:08:53 +0530

    tested

--- lect-32/TopViewOfTree.cpp
+++ lect-32/TopViewOfTree.cpp
@@ -3,19 +3,18 @@
 #include<queue>
 #include<map>
 using namespace std;
-
+//here create node
 class Node {
 public:
     int data;
     Node* left;
     Node* right;
-
     Node(int data) {
         this->data = data;
         left = right = NULL;
     }
 };
-
+// building tree
 static int idx = -1;
 
 Node* Build_Trees(const vector<int>& nodes) {
@@ -65,6 +64,8 @@ void TopViewOfTree(Node* root) {
     }
     cout << endl;
 }
+
+// main
 
 int main() {
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
