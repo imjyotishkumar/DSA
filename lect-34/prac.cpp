9748ecaca1cfe6809b6efb812a57676795d59105
jyotispro <kumarjyotish67303@gmail.com>
2025-02-13 01:53:48 +0530

    bst part 2

--- lect-34/prac.cpp
+++ lect-34/prac.cpp
@@ -1,90 +1,58 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Node{
-    public:
-    int data;
-    Node* left;
-    Node* right;
-    Node(int data){
-        this->data = data;
-        left = right = NULL;
-    }
+public:
+int data;
+Node*left;
+Node*right;
+Node(int data){
+    this->data = data;
+}
 };
 
 Node* insert(Node* root, int val){
-    if(root == NULL){
+
+    if( root == NULL ){
         root = new Node(val);
         return root;
     }
-    if(root->data > val){
-        root->left = insert(root->left , val);
+     
+    if( root->data > val ){
+        root->left = insert( root->left , val );
     } else{
-        root->right = insert(root->right,val);
-    }
-    return root;
-}
-
-Node* BuildBST(int *arr, int n){
-    Node* root = NULL;
-    for(int i = 0; i < n; i++){
-        root = insert(root,arr[i]);
+        root->right = insert( root->right , val );
     }
 
     return root;
 }
 
-Node* getinordersuccessor(Node*root){
-    while (root->left!=NULL)
-    {
-        root = root->left;
-    }
-    return root;
-    
+Node* BuildBST(int arr[], int n){
+Node* root = NULL;
+for(int i = 0; i < n; i++){
+    root = insert( root , arr[i] );
+}
+return root;
 }
 
-Node* delNode(Node*root, int val){
-    if(root == NULL){
-        return NULL;
-    }
-
-    if(root->data > val){
-        root->left = delNode(root->left, val);
-    } else if(root->data<val){
-        root->right = delNode(root->right, val);
-    } else{
-        if(root->left == NULL && root->right == NULL){
-            delete root;
-            return NULL;
-        }
-        if(root->left == NULL || root->right == NULL){
-            return root->left == NULL ? root->right:root->left;
-        }
-        Node* IS = getinordersuccessor(root->right);
-        root->data = IS->data;
-        root->right = delNode(root->right,IS->data);
-        return root;
-    }
-}
-
-void inorder(Node* root){
+void inorder(Node*root){
     if(root == NULL){
         return;
-    } 
+    }
     inorder(root->left);
     cout<<root->data<<" ";
     inorder(root->right);
 }
 
 int main(){
+    
+    int arr[] = {3,2,1,4,5};
+    int n = sizeof(arr)/sizeof(int);
+    Node*root = BuildBST(arr,n);
+    cout<<n<<endl;
+    inorder(root);
 
-int arr[] = {8,5,3,1,4,6,10,11,14};
-int n = sizeof(arr) / sizeof(int);
-Node* root = BuildBST(arr,n);
-inorder(root);
-cout<<endl;
-delNode(root,4);
-inorder(root);
 
     return 0;
 }
--- lect-34/validateBST.cpp
+++ lect-34/validateBST.cpp
@@ -1,77 +1,76 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-
-     public:
-     int data;
-     Node* left;
-     Node* right;
-     Node(int data){
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data) {
         this->data = data;
         left = right = NULL;
-     }
-
+    }
 };
 
-Node* insert(Node*root, int val){
-    if(root == NULL){
+Node* insert(Node* root, int val) {
+    if (root == NULL) {
         root = new Node(val);
         return root;
     }
 
-    if( val < root->data){
+    if (val < root->data) {
         root->left = insert(root->left, val);
-    } else{
+    } else {
         root->right = insert(root->right, val);
     }
 
     return root;
 }
 
-Node* BuildBST(int arr[] , int n){
+Node* BuildBST(int arr[], int n) {
     Node* root = NULL;
 
-    for(int i = 0; i< n; i++){
+    for (int i = 0; i < n; i++) {
         root = insert(root, arr[i]);
     }
 
     return root;
 }
 
-void inorder(Node*root){
-    if(root== NULL){
+void inorder(Node* root) {
+    if (root == NULL) {
         return;
     }
 
     inorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     inorder(root->right);
 }
 
-bool validateHelper(Node*root,Node*min, Node* max){
-    if(root == NULL){
+bool validateHelper(Node* root, Node* min, Node* max) {
+    if (root == NULL) {
         return true;
     }
-    if(min!=NULL&&root->data<min->data){
+    if (min != NULL && root->data <= min->data) {
         return false;
     }
-    if(max!=NULL&&root->data>max->data){
+    if (max != NULL && root->data >= max->data) {
         return false;
     }
-    return validateHelper(root->left,min,root) && validateHelper(root->right,root,max);
+    return validateHelper(root->left, min, root) && validateHelper(root->right, root, max);
 }
 
-bool validateBST(Node* root){
-    return validateHelper(root,NULL,NULL);
+bool validateBST(Node* root) {
+    return validateHelper(root, NULL, NULL);
 }
 
-int main(){
-    int arr[] = {5,1,4,3,7};
-    int n = sizeof(arr)/sizeof(int);
-    Node* root = BuildBST(arr,n);
+int main() {
+    int arr[] = {3, 2, 1, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    Node* root = BuildBST(arr, n);
     inorder(root);
-    cout<<endl;
-    cout<<validateBST(root);
+    cout << endl;
+    cout << root->data << endl;
+    cout << validateBST(root);
     return 0;
 }
